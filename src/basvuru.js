4031c0c13a7f3b3d29f295dd2fe0298d62eabe3c
Furkan <96385211+violencedev@users.noreply.github.com>
2022-05-04 17:40:32 +0300

    Add files via upload

---
+++ src/basvuru.js
@@ -0,0 +1,8 @@
+const toggleBtn2 = document.querySelector('.toggle-btn'),
+navBar2 = document.querySelector('.navbar');
+
+console.log(togg)
+
+toggleBtn2.addEventListener('click', () => {
+    navBar2.classList.toggle('active');
+})
