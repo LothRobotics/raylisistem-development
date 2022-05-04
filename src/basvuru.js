0af7801d8f59dfeb395f092acc3113fce971e036
FurkanEsen Esen <furkanesen39@gmail.com>
2022-05-04 18:35:59 +0300

    master

--- src/basvuru.js
+++ src/basvuru.js
@@ -1,7 +1,6 @@
 const toggleBtn2 = document.querySelector('.toggle-btn'),
 navBar2 = document.querySelector('.navbar');
 
-console.log(togg)
 
 toggleBtn2.addEventListener('click', () => {
     navBar2.classList.toggle('active');
